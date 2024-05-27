8474363171912ea331118d7fb0cc0635f60c1bbd
Nadezhda Nikitina <109433215+Nadezhda-97@users.noreply.github.com>
2024-05-28 02:01:44 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+const calc = document.querySelector('.calc');
+const result = document.querySelector('#result');
 
+calc.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('calc_btn')) {
+    return;
+  }
+
+  const value = e.target.innerText;
+
+  switch(value) {
+    case 'C':
+      result.innerText = '';
+      break;  
+
+    case '=':
+      result.innerText = eval(result.innerText).toFixed(2);
+      break;    
+
+    default:
+      result.innerText += value;
+  }
+});
