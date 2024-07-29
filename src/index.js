b8f352fe3170b3482c0cdead7a3e6994b79a41dc
Nadezhda Nikitina <109433215+Nadezhda-97@users.noreply.github.com>
2024-07-30 01:10:59 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,92 @@
-const calc = document.querySelector('.calc');
-const result = document.querySelector('#result');
+const out = document.querySelector('.calc-screen p');
+const buttons = document.querySelector('.buttons');
+const cleanButton = document.querySelector('.clean');
 
-calc.addEventListener('click', (e) => {
-  if (!e.target.classList.contains('calc_btn')) {
+let firstItem = '';
+let secondItem = '';
+let sign = '';
+let isFinish = false;
+
+const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const signs = ['+', '-', 'x', '/'];
+
+const cleanAll = () => {
+  firstItem = '';
+  secondItem = '';
+  sign = '';
+  isFinish = false;
+  out.textContent = '0';
+};
+
+cleanButton.addEventListener('click', () => cleanAll());
+
+buttons.addEventListener('click', (e) => {
+  // если клик не на кнопку
+  if (!e.target.classList.contains('btn')) return;
+  
+  // если клик на кнопку "очистить"
+  if (e.target.classList.contains('clean')) return;
+
+  out.textContent = '';
+
+  // получить содержимое нажатой кнопки (какую кнопку нажали)
+  const buttonContent = e.target.textContent;
+
+  // если нажаты кнопки 0-9 или "."
+  if (digits.includes(buttonContent)) {
+    if (secondItem === '' && sign === '') {
+      firstItem += buttonContent;
+      out.textContent = firstItem;
+    } else if (firstItem !== '' && secondItem !== '' && isFinish) {
+      secondItem = buttonContent;
+      isFinish = false;
+      out.textContent = secondItem;
+    } else {
+      secondItem += buttonContent;
+      out.textContent = secondItem;
+    }
+
     return;
   }
 
-  const value = e.target.innerText;
+  // если нажат знак операции
+  if (signs.includes(buttonContent)) {
+    sign = buttonContent;
+    out.textContent = sign;
+  }
 
-  switch(value) {
-    case 'C':
-      result.innerText = '';
-      break;  
+  // если нажато "="
+  if (buttonContent === '=') {
+    if (secondItem === '') {
+      secondItem = firstItem;
+    }
 
-    case '=':
-      result.innerText = eval(result.innerText).toFixed(2);
-      break;    
+    switch (sign) {
+      case '+':
+        firstItem = Number(firstItem) + Number(secondItem);
+        break;
+      case '-':
+        firstItem = Number(firstItem) - Number(secondItem);
+        break;
+      case 'x':
+        firstItem = Number(firstItem) * Number(secondItem);
+        break;
+      case '/':
+        if (secondItem === '0') {
+          out.textContent = 'Error';
+          firstItem = '';
+          secondItem = '';
+          sign = '';
+          return;
+        }
+        firstItem = Number(firstItem) / Number(secondItem);
+        break;
+      default:
+        out.textContent = 'Oops';
+        console.error('Something was wrong with signs');
+    }
 
-    default:
-      result.innerText += value;
+    isFinish = true;
+    out.textContent = firstItem;
   }
 });
